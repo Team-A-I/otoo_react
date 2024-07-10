e81e85a1be723bc6564266b0213f6d62931a3b72
catapracts <catapracts@gmail.com>
2024-07-10 14:19:05 +0900

    Feature/SY/우정페이지 제작

--- src/components/friendship/FileUploadFriendship.js
+++ src/components/friendship/FileUploadFriendship.js
@@ -1,136 +1,126 @@
-import React, { useState, useRef } from 'react';
-import axios from 'axios';
+import React, { useState, useCallback } from 'react';
+import { Container, Typography, Box, Grid, ThemeProvider } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import { Button, Container, TextField, Typography, Box, Grid, ThemeProvider} from '@mui/material';
-import theme from '../../theme';
+import FriendshipButton from './FriendshipButton'; // 새로 만든 버튼 컴포넌트
+import theme from "../../theme";
 
-const FileUploadFriendship = () => {
-  const [file, setFile] = useState(null);
-  const fileInputRef = useRef(null);
+// 변수 정의
+const cardMaxWidth = 700;
+const imageHeight = 400;
+const imageSrc = "/otoo_react/images/friendship-1.jpg";
+const imageAlt = "friendship-1";
+const friendshipmainText = "Best\nFriend\nForever\n";
+const cardContentText = "#찐친테스트\n#우정파괴\n#테스트는 테스트일 뿐^^";
+const btnUploadLabel = "카카오톡 파일 업로드";
+const btnResultLabel = "결과 보러가기";
+
+
+
+const FileUpload = () => {
+  const [file, setFile] = useState(null);// eslint-disable-next-line
+  const [jsonContent, setJsonContent] = useState(null);
   const navigate = useNavigate();
 
-  const handleFileChange = (event) => {
-      setFile(event.target.files[0]);
+  const handleFileChange = useCallback((event) => {
+    setFile(event.target.files[0]);
+    console.log("File selected:", event.target.files[0]);
+  }, []);
+
+  const handleFileRead = useCallback((event) => {
+    const content = event.target.result;
+    try {
+      const json = { text: content }; // 전체 텍스트를 하나의 'text' 필드에 저장
+      setJsonContent(json);
+      console.log("JSON Content:", json);
+      navigate('/loading-friendship', { state: { jsonContent: json } });
+    } catch (error) {
+      console.error("Error parsing file:", error);
+    }
+  }, [navigate]);
+
+  const handleFileUpload = () => {
+    if (!file) {
+      console.log("No file selected");
+      return;
+    }
+    console.log("handleFileUpload called");
+    console.log("File exists:", file);
+    const reader = new FileReader();
+    reader.onload = handleFileRead;
+    reader.readAsText(file);
   };
 
-  const handleButtonClick = () => {
-      fileInputRef.current.click();
-  };
-
-  const handleSubmit = async (event) => {
-      event.preventDefault();
-      if (!file) {
-          return;
-      }
-
-      const reader = new FileReader();
-      reader.onload = async (e) => {
-          const content = e.target.result;
-          const userId = "example_user_id"; // 실제 사용자 ID를 여기에 넣어야 합니다.
-          const data = {
-              user_id: userId,
-              content: content
-          };
-          console.log("data",data)
-
-          try {
-              navigate('/loading-love');
-              const response = await axios.post('http://localhost:8080/api/friendship/upload', data, {
-                  headers: {
-                      'Content-Type': 'application/json',
-                  },
-              });
-              console.log("response", response)
-              console.log("response.data", response.data)
-              navigate('/result-friendship', { state: { result: response.data } });
-          } catch (error) {
-              console.error('Error uploading file:', error);
-          }
-      };
-      reader.readAsText(file);
-  };
 
   return (
-      <Container maxWidth="xl">
-        <ThemeProvider theme={theme}>
+    <Container maxWidth="xl">
+      <ThemeProvider theme={theme}>
         <div style={{ fontFamily: theme.typography.fontFamily }}>
-        <Box className="container">
-        <Grid container spacing={1}>
-          <Grid item xs={12} sm={6}container
-          alignItems="center">
-            <Typography variant="hhue"
-            color="dyellow" className="lovemain-text">
-              Best{'\n'}Friend{'\n'}Forever{'\n'}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-          <Card sx={{ maxWidth: 700 }}>
-            <CardHeader/>
-            <CardMedia
-              component="img"
-              height="400"
-              image="/otoo_react/images/lovemain.jpg"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="h3_mid" color="text.secondary">
-                상대방과 나눈 간지러운 대화를 넣어주세요.<br/> 누가 더 좋아하는지 저희가 판단해드릴게요.<br/> 판단의 기준과 함께 서로의 관심사를 같이 보여드릴게요.<br/> 지금 무슨 생각을 하고 있을까요?
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Box className="button-container" mt={12}>
-            <input
-                type="file"
-                ref={fileInputRef}
-                style={{ display: 'none' }}
-                onChange={handleFileChange}
-            />
-            <Button variant="contained"
-              component="span" 
-              size="large"
-              sx={{ 
-                width: '200px', 
-                height: '50px',
-                mr: 2, 
-                backgroundColor: theme.palette.dyellow, 
-                color: theme.palette.gray700,
-                '&:hover': {
-                  backgroundColor: theme.palette.dyellow, // 호버 시 배경색 변경
-                }
-              }}
-              onClick={handleButtonClick}>
-                카카오톡 데이터 입력하기
-            </Button>
-            <Button variant="contained"
-              type="submit"
-              component="span"
-              size="large" 
-              disabled={!file}
-              sx={{ 
-                mr: 2, 
-                backgroundColor: theme.palette.dyellow,
-                color: theme.palette.gray700,
-                '&:hover': {
-                  backgroundColor: theme.palette.dyellow, // 호버 시 배경색 변경
-                }
-                }}
-                onClick={handleSubmit}>
-                결과 보러가기
-            </Button>
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '12vh' }}>
+            <Grid container>
+              <Grid item xs={12} sm={6} container alignItems="center">
+                <Typography
+                  variant="hbig"
+                  sx={{
+                    fontWeight: 900,
+                    whiteSpace: 'pre-line',
+                    fontSize: '95px',
+                    color: '#0495D2'
+                  }}
+                >
+                  {friendshipmainText}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Card sx={{ maxWidth: cardMaxWidth }}>
+                  <CardHeader />
+                  <CardMedia
+                    component="img"
+                    height={imageHeight}
+                    image={imageSrc}
+                    alt={imageAlt}
+                  />
+                  <CardContent>
+                    <Typography variant="h3_mid" color="text.secondary">
+                      {cardContentText}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '4vh' }}>
+                  <input
+                    accept=".txt"
+                    style={{ display: 'none' }}
+                    id="raised-button-file"
+                    type="file"
+                    onChange={handleFileChange}
+                  />
+                  <label htmlFor="raised-button-file">
+                    <FriendshipButton
+                      label={btnUploadLabel}
+                      onClick={() => {}}
+                      disabled={false} // 파일 선택 여부와 관계없이 기본 색상을 유지하도록
+                      className="conflict-btn-upload"
+                    />
+                  </label>
+                  <FriendshipButton
+                    label={btnResultLabel}
+                    onClick={handleFileUpload}
+                    disabled={!file}
+                    className="conflict-btn-result"
+                  />
+                </Box>
+              </Grid>
+            </Grid>
           </Box>
-        </Grid>
-        </Box>
         </div>
-        </ThemeProvider>
-      </Container>
+      </ThemeProvider>
+    </Container>
   );
 };
 
-export default FileUploadFriendship;
+export default FileUpload;
---
+++ src/components/friendship/ResultFriendship.js
@@ -0,0 +1,318 @@
+import React from 'react';
+import { Box, Typography, Grid, Paper, Container, Table, TableHead, TableRow, TableCell, TableBody, ThemeProvider, useMediaQuery, useTheme , Card , CardContent} from '@mui/material';
+import { useLocation, useNavigate } from 'react-router-dom';
+import theme from "../../theme";
+import 'chart.js/auto'; 
+import { CustomPaper, AttributeCard, TitleSection } from '../conflict/CommonComponentsConflict';
+
+const ResultFriendship = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { jsonData } = location.state || {};
+  const data = jsonData ? JSON.parse(jsonData.response.replace(/```json\n|```/g, '')) : {};
+  const theme1 = useTheme();
+  const isSmallScreen = useMediaQuery(theme1.breakpoints.down('sm'));
+
+  // console.log("jsonData = "+jsonData)
+  // console.log("==================")
+  // console.log(jsonData)
+  // console.log("==================")
+  // console.log(location.state)
+  // console.log("==================")
+  // console.log(location.state.friendship_likeability)
+  // console.log("==================")
+  // console.log(jsonData.friendship_likeability)
+  // console.log("==================")
+  // console.log(data.friendship_likeability)
+  // console.log("==================")
+  // const test = Object.keys(data.friendship_likeability || {});
+  // console.log(test)
+  // console.log(test[0])
+  // console.log(test[1])
+  // console.log("==================")
+  // console.log(data.friendship_likeability[test])
+  // console.log("==================")
+  // console.log(data.friendship_likeability[test[0]])
+  // console.log(data.friendship_likeability[test[0]].score)
+  // console.log(data.friendship_likeability[test[0]].reason)
+
+  const test = Object.keys(data.friendship_likeability || {});
+
+  if(data.friendship_likeability[test[0]].score>=80 || data.friendship_likeability[test[1]].score>=80)
+  {
+     navigate('/result-friendship-to-love', { state: { data } });
+  }
+
+  // console.log("=================================")
+  // console.log(data)
+  // console.log("=================================")
+  // console.log(data.total_score)
+  // console.log(data.total_score["지영"])
+  // console.log("=================================")
+  // console.log(data.sacrifice)
+  // console.log(data.sacrifice["지영"].score)
+  // console.log(data.sacrifice["지영"].reason)
+  // console.log("=================================")
+  // console.log(data.comfortable)
+  // console.log(data.comfortable["하영"].score)
+  // console.log(data.comfortable["하영"].reason)
+  // console.log("=================================")
+  // console.log(data.betrayer)
+  // console.log(data.betrayer["지영"].score)
+  // console.log(data.betrayer["지영"].reason)
+  // console.log("=================================")
+  // console.log(data.Biggest_Sentimental)
+  // console.log(data.Biggest_Sentimental["지영"])
+  // console.log(data.Biggest_Sentimental["지영"][0])
+  // console.log("=================================")
+  // console.log(Object.keys(data))
+  // console.log(Object.keys(data).length)
+  // console.log("=================================")
+
+  //비율별 날씨아이콘
+  const getImageByPercentage = (percentage) => {
+    if (percentage >= 0 && percentage <= 20) return '/otoo_react/images/무지개.png';
+    if (percentage >= 21 && percentage <= 40) return '/otoo_react/images/맑음.png';
+    if (percentage >= 41 && percentage <= 50) return '/otoo_react/images/약간흐림.png';
+    if (percentage >= 51 && percentage <= 60) return '/otoo_react/images/구름.png';
+    if (percentage >= 61 && percentage <= 80) return '/otoo_react/images/비.png';
+    if (percentage >= 81 && percentage <= 100) return '/otoo_react/images/낙뢰.png';
+    return '';
+  };
+
+  //비율별 배경이미지&텍스트
+  const getStyleByTotalscore = (percentage) => {
+    if (percentage >= 0 && percentage <= 10) {
+      return {
+        imageUrl: '/otoo_react/images/낙뢰하늘사진.jpg',
+        color: theme.palette.gray200
+      };
+    } else if (percentage >= 11 && percentage <= 20) {
+      return {
+        imageUrl: '/otoo_react/images/비하늘사진.jpg',
+        color: theme.palette.gray900
+      };
+    } else if (percentage >= 21 && percentage <= 45) {
+      return {
+        imageUrl: '/otoo_react/images/흐린하늘사진.png',
+        color: theme.palette.gray900
+      };
+    } else if (percentage >= 46 && percentage <= 100) {
+      return {
+        imageUrl: '/otoo_react/images/맑은하늘사진.jpg',
+        color: theme.palette.gray200
+      };
+    } else {
+      return {
+        imageUrl: '',
+        color: '#000000' // 기본 색상 (검정색)
+      };
+    }
+  };
+
+  // 결과페이지 타이틀
+  const titleText = () => {
+    const names = Object.keys(data.friendship_total_score || {});
+    
+    return names.length === 2 
+      ? `${names[0]}님과<br />${names[1]}님의<br />판결 결과입니다` 
+      : '판결 결과입니다';
+  };
+
+  // 전체 통계 
+  const renderWrongPercentage = () => {
+    const names = Object.keys(data.friendship_total_score || {});
+    return (
+      <Grid item xs={12}>
+        <Paper elevation={3} style={{ padding: '24px', backgroundImage: 'url(/otoo_react/images/맑은배경.png)', backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '320px', position: 'relative', borderRadius:'35px' }}>
+          <Grid container spacing={3} alignItems="center">
+            <Grid item xs={12} sm={4}>
+              <Grid container justifyContent="center" alignItems="center" style={{ height: '100%' }}>
+                <Typography variant="hc_bold" color="dyellow" gutterBottom >
+                  오늘의 <br /> 우정 일기예보 입니다. <br />누가 더 우정하는지 <br /> 알아 보겠습니다.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <Grid container spacing={2} justifyContent={isSmallScreen ? 'center' : 'flex-end'}>
+                {names.map(name => (
+                  <Grid item xs={12} sm={5} key={name} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Card style={{ height: '100%', width: '100%', borderRadius: '15px', minHeight: '320px' }}>
+                      <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <Typography variant="title_bold" color="gray900" gutterBottom mt={3}>{name}</Typography>
+                        <img src={getImageByPercentage(data.friendship_total_score[name])} alt={name} style={{ width: '100%', height: 'auto', maxHeight: '150px', objectFit: 'cover', marginBottom: '16px' }} />
+                        <Typography variant="h1_bold" color="gray600" style={{ fontSize: '2vw' }}>{data.friendship_total_score[name]}%</Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+          <img src="/otoo_react/images/weathercaster.png" alt="기상캐스터" style={{ position: 'absolute', bottom: '-17px', left: '-115px', width: '250px', height: 'auto' }} />
+        </Paper>
+      </Grid>
+    );
+  };
+
+
+  // 세부분석
+  const renderPersonData = (name) => {
+    const sacrificeScore = data.friendship_sacrifice[name].score;
+    const sacrificeReason = data.friendship_sacrifice[name].reason;
+    const comfortableScore = data.friendship_comfortable[name].score;
+    const comfortableReason = data.friendship_comfortable[name].reason;
+    const betrayerScore = data.friendship_betrayer[name].score;
+    const betrayerReason = data.friendship_betrayer[name].reason;
+    const totalScore = data.friendship_total_score[name];
+    const style = getStyleByTotalscore(totalScore);
+
+  
+    const attributes = [
+      {
+        title: '누가 총을 대신 맞아줄것인가?',
+        percentage: sacrificeScore,
+        tooltip: sacrificeReason,
+      },
+      {
+        title: '흔들리지 않는 편안한 침대같은 사람',
+        percentage: comfortableScore,
+        tooltip: comfortableReason,
+      },
+      {
+        title: '뒷 통수 칠 사람',
+        percentage: betrayerScore,
+        tooltip: betrayerReason,
+      },
+    ];
+  
+    return (
+      <Grid item xs={12} key={name}>
+        <CustomPaper>
+          <Grid container spacing={3} alignItems="center">
+            <Grid item xs={12} sm={4} textAlign="center">
+              <Paper
+                elevation={3}
+                style={{
+                  padding: '16px',
+                  backgroundImage: `url(${style.imageUrl})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  height: '100%',
+                  minHeight: '260px',
+                  borderRadius: '15px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h1_bold" color={style.color} gutterBottom>
+                  {name}님의 <br /> 우정도
+                </Typography>
+                <Typography variant="h1_bold" color={style.color} gutterBottom>
+                  {totalScore}%
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <Grid container spacing={3} alignItems="stretch">
+                {attributes.map((attr, index) => (
+                  <Grid item xs={4} key={index}>
+                    <AttributeCard
+                      title={attr.title}
+                      percentage={attr.percentage}
+                      tooltip={attr.tooltip}
+                      imageSrc={getImageByPercentage(attr.percentage)}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+        </CustomPaper>
+      </Grid>
+    );
+  };
+
+  
+  // Top5 키워드
+  const renderPriorityKeywords = () => {
+    const names = Object.keys(data.friendship_Biggest_Sentimental || {});
+    const renderTable = (name, keywords, color) => (
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" colSpan={2}>{name}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center">순위</TableCell>
+            <TableCell align="center">감정</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {keywords.map((keyword, index) => (
+            <TableRow key={index}>
+              <TableCell align="center">{index + 1}위</TableCell>
+              <TableCell align="center" style={{ backgroundColor: color }}>{keyword}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    );
+  
+    const images = [
+      { src: "/otoo_react/images/yumi2.png", width: '110px', height: 'auto' }, // 첫 번째 이미지 크기
+      { src: "/otoo_react/images/yumi.png", width: '90px', height: 'auto' }  // 두 번째 이미지 크기
+    ];
+  
+    return (
+      <Grid item xs={12}>
+        <CustomPaper style={{ position: 'relative' }}>
+          <Typography variant="title_bold" gutterBottom>감정 순위 키워드</Typography>
+          <Grid container spacing={3}>
+            {names.map((name, index) => (
+              <Grid item xs={12} sm={6} key={name} style={{ position: 'relative' }}>
+                {renderTable(name, data.friendship_Biggest_Sentimental[name], index === 0 ? '#ECD3D8' : '#0495D2')}
+                <img
+                  src={images[index].src}
+                  alt="일러스트"
+                  style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    left: index === 1 ? '10px' : 'auto',
+                    right: index === 0 ? '10px' : 'auto',
+                    width: images[index].width,
+                    height: images[index].height,
+                    zIndex: 1
+                  }}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </CustomPaper>
+      </Grid>
+    );
+  };
+
+
+  //전체 리턴
+  return (
+    <Container maxWidth="lg">
+      <ThemeProvider theme={theme}>
+        <div style={{ fontFamily: theme.typography.fontFamily }}>
+          <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="100vh">
+            <Grid container spacing={3} mt={5}>
+              {data.friendship_total_score && <TitleSection titleText={titleText()} imgSrc="/otoo_react/images/friendship-5.jpg" imgAlt="결과 이미지" />}
+              {data.friendship_total_score && renderWrongPercentage()}
+              {Object.keys(data.friendship_total_score || {}).map((name) => renderPersonData(name))}
+              {data.friendship_Biggest_Sentimental && renderPriorityKeywords()}
+            </Grid>
+          </Box>
+        </div>
+      </ThemeProvider>
+    </Container>
+  );
+};
+
+export default ResultFriendship;
