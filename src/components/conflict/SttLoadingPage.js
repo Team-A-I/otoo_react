78f3cbf866c608b33020e7af00a9e7bf1c5c9831
배정현 <bae4445@gmail.com>
2024-07-23 19:08:11 +0900

    [Feature] 로딩페이지 업데이트

--- src/components/conflict/SttLoadingPage.js
+++ src/components/conflict/SttLoadingPage.js
@@ -56,8 +56,8 @@ const SttLoadingPage = () => {
   }, [jsonContent, navigate, usercode]);
 
   return (
-    <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', padding: 0, margin: 0 }}>
-      <img src="/images/moonchul_dance.gif" alt="loading" style={{ width: '60%', height: '60%', objectFit: 'cover' }} />
+    <Container sx={{ display: 'flex', marginTop:"35px", justifyContent:'center'}}>
+      <img src="/images/moonchul_dance.gif" alt="loading" />
     </Container>
   );
 };
