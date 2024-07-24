cc3ba57b11ca1c3efec41681e6c059496ea4284f
khe8895 <khe8895@gachon.ac.kr>
2024-07-24 17:54:57 +0900

    [Feature]방명록어드민페이지 제작, APP리팩토링

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';// eslint-disable-next-line
-import { BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -29,7 +29,6 @@ import GoogleLogin from './pages/users/GoogleLogin';
 import NaverLogin from './pages/users/NaverLogin';
 import ForgetPassword from './pages/users/ForgetPassword';
 import ResetPassword from './pages/users/ResetPassword';
-// eslint-disable-next-line
 import PrivateRoute from './components/PrivateRoute';
 import QnaChatbot from './components/modal/QnaChatbot';
 
@@ -44,66 +43,73 @@ import Board from './pages/Board';
 
 import AdminUser from './pages/admin/AdminUser';
 import AdminHeader from './pages/admin/AdminHeader';
+import AdminBoard from './pages/admin/AdminBoard';
 
+const adminRoutes = [
+  { path: '/admin-user', element: <AdminUser />, roles: ['ROLE_ADMIN'] },
+  { path: '/admin-board', element: <AdminBoard />, roles: ['ROLE_ADMIN'] },
+];
+
+const generalRoutes = [
+  { path: '/', element: <Home /> },
+  { path: '/chatbot', element: <ChatBot /> },
+  { path: '/qna-chatbot', element: <QnaChatbot /> },
+  { path: '/upload-conflict', element: <ConflictUpload /> },
+  { path: '/loading-conflict', element: <LoadingPage /> },
+  { path: '/result-conflict', element: <ResultPage /> },
+  { path: '/loading-love', element: <LoadingLove /> },
+  { path: '/result-love', element: <ResultLove /> },
+  { path: '/upload-love', element: <UploadLove /> },
+  { path: '/loading-friendship', element: <LoadingFriendship /> },
+  { path: '/result-friendship', element: <ResultFriendship /> },
+  { path: '/result-friendship-to-love', element: <ResultFriendshipToLove /> },
+  { path: '/upload-friendship', element: <UploadFriendship /> },
+  { path: '/emotionReportPage', element: <EmotionReportPage /> },
+  { path: '/emotionReportLoadingPage', element: <EmotionReportLoadingPage /> },
+  { path: '/user-login', element: <UserLoginPage /> },
+  { path: '/user-signup', element: <SignUpPage /> },
+  { path: '/googlelogin', element: <GoogleLogin /> },
+  { path: '/naverLogin', element: <NaverLogin /> },
+  { path: '/forgetPassword', element: <ForgetPassword /> },
+  { path: '/resetPassword', element: <ResetPassword /> },
+  { path: '/stt-result', element: <SttResult /> },
+  { path: '/recorder', element: <Recorder /> },
+  { path: '/stt-loading', element: <SttLoadingPage /> },
+  { path: '/stt', element: <SttUpload /> },
+  { path: '/streaming', element: <Streaming /> },
+  { path: '/recorder-loading', element: <RecorderLoading /> },
+  { path: '/board', element: <Board /> },
+];
 
 const MainApp = () => {
   const location = useLocation();
 
-    // 경로에 따라 헤더를 결정
-    const HeaderComponent = () => {
-      if (location.pathname === '/admin-user' || location.pathname === '/admin-analyze') {
-        return <AdminHeader />;
-      }
-      return <Header />;
-    };
-    
+  const HeaderComponent = () => {
+    if (location.pathname.startsWith('/admin')) {
+      return <AdminHeader />;
+    }
+    return <Header />;
+  };
+
   return (
     <>
       <HeaderComponent />
       <Box>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route 
-            path="/admin-user" 
-            element={
-              <PrivateRoute roles={['ROLE_ADMIN']}>
-                <AdminUser />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/chatbot" element={<ChatBot />} />
-          <Route path="/qna-chatbot" element={<QnaChatbot />} />
-          <Route path="/upload-conflict" element={<ConflictUpload />} />
-          <Route path="/loading-conflict" element={<LoadingPage />} />
-          <Route path="/result-conflict" element={<ResultPage />} />
-
-          <Route path="/loading-love" element={<LoadingLove />} />
-          <Route path="/result-love" element={<ResultLove />} />
-          <Route path="/upload-love" element={<UploadLove />} />
-
-          <Route path="/loading-friendship" element={<LoadingFriendship />} />
-          <Route path="/result-friendship" element={<ResultFriendship />} />
-          <Route path="/result-friendship-to-love" element={<ResultFriendshipToLove />} />
-          <Route path="/upload-friendship" element={<UploadFriendship />} />
-
-          <Route path="/emotionReportPage" element={<EmotionReportPage />} />
-          <Route path="/emotionReportLoadingPage" element={<EmotionReportLoadingPage />} />
-
-          <Route path="/user-login" element={<UserLoginPage />} />
-          <Route path="/user-signup" element={<SignUpPage />} />
-          <Route path="/googlelogin" element={<GoogleLogin />} />
-          <Route path="/naverLogin" element={<NaverLogin />} />
-          <Route path="/forgetPassword" element={<ForgetPassword />} />
-          <Route path="/resetPassword" element={<ResetPassword />} />
-
-          <Route path='/stt-result' element={<SttResult />} />
-          <Route path='/recorder' element={<Recorder />} />
-          <Route path='/stt-loading' element={<SttLoadingPage />} />
-          <Route path='/stt' element={<SttUpload />} />
-          <Route path='/streaming' element={<Streaming />} />
-          <Route path='/recorder-loading' element={<RecorderLoading />} />
-          <Route path='/board' element={<Board />} />
-
+          {adminRoutes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={
+                <PrivateRoute roles={route.roles}>
+                  {route.element}
+                </PrivateRoute>
+              }
+            />
+          ))}
+          {generalRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
         </Routes>
       </Box>
     </>
---
+++ src/pages/admin/AdminBoard.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Box, Typography, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import theme1 from '../../theme';
+
+const API_URL = 'https://gnat-suited-weekly.ngrok-free.app/api/posts';
+const HEADERS = {
+  'Content-Type': 'application/json',
+  'ngrok-skip-browser-warning': '69420',
+};
+
+const AdminBoard = () => {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [postsPerPage, setPostsPerPage] = useState(9);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get(API_URL, { headers: HEADERS });
+      const sortedPosts = response.data.sort((a, b) => new Date(b.date) - new Date(a.date));
+      setPosts(sortedPosts);
+    } catch (error) {
+    }
+  };
+
+  const handleDeletePost = async (postId) => {
+    try {
+      await axios.delete(`${API_URL}/${postId}`, { headers: HEADERS });
+      setPosts(posts.filter(post => post.id !== postId));
+    } catch (error) {
+    }
+  };
+
+  const handlePageChange = (event, newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPostsPerPage(parseInt(event.target.value, 10));
+    setCurrentPage(0);
+  };
+
+  const indexOfLastPost = currentPage * postsPerPage + postsPerPage;
+  const indexOfFirstPost = currentPage * postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  return (
+    <ThemeProvider theme={theme1}>
+      <Box sx={{ padding: 2 }}>
+        <Typography variant="h4" gutterBottom>방명록 관리</Typography>
+        <br></br>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {['ID', '작성자', '날짜', '설명', '삭제'].map(header => (
+                  <TableCell key={header} sx={{ backgroundColor: 'black', color: 'white', fontWeight: 'bold' }}>
+                    {header}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {currentPosts.map((post) => (
+                <TableRow key={post.id}>
+                  <TableCell>{post.id}</TableCell>
+                  <TableCell>{post.author}</TableCell>
+                  <TableCell>{post.date}</TableCell>
+                  <TableCell>{post.description}</TableCell>
+                  <TableCell>
+                    <Button variant="contained" color="error" onClick={() => handleDeletePost(post.id)}>
+                      삭제
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[9, 25, 100]}
+          component="div"
+          count={posts.length}
+          rowsPerPage={postsPerPage}
+          page={currentPage}
+          onPageChange={handlePageChange}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export default AdminBoard;
